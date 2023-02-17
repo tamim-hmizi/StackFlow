2564795107594b40c7cb286d38d2f6a9016370cd
Aminelarbi <mohamedamine.larbi@esprit.tn>
2023-02-18 00:58:04 +0100

    CRUD TERMINER

--- src/services/IService.java
+++ src/services/IService.java
@@ -1,15 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package services;
 
+import java.sql.SQLException;
 import java.util.List;
 
+/**
+ *
+ * @author larbi
+ */
 public interface IService<T> {
-
-    public void Create(T t);
-
-    public void Read(T t);
-
-    public void Update(T t);
-
-    public List<T> Delete(T t);
-
+    
+    public void Create(T t) throws SQLException;
+    public void Update(T t) throws SQLException;
+    public void Delete(T t) throws SQLException;
+    public List<T> Read(T t) throws SQLException;
+    
 }
--- src/utils/MyDB.java
+++ src/utils/MyDB.java
@@ -3,7 +3,7 @@ package utils;
 import java.sql.*;
 
 public class MyDB {
-    private String url = "jdbc:mysql://localhost:3306/esprit", username = "root", password = "";
+    private String url = "jdbc:mysql://localhost:3306/stackFlow", username = "root", password = "";
     private Connection cnx;
     private static MyDB instance;
 
